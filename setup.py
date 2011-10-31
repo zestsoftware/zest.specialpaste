80d91ba44ca1f40dae071bdacf237452ba23105f
Maurits van Rees <maurits@vanrees.org>
2011-10-31 14:10:57 +0100

    initial package layout

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0dev'
+
+setup(name='zest.specialpaste',
+      version=version,
+      description="Special paste action with extra options",
+      long_description=open("README.txt").read() + "\n" +
+                       open("CHANGES.txt").read(),
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='copy paste workflow state',
+      author='Zest Software',
+      author_email='info@zestsoftware.nl',
+      url='https://github.com/zestsoftware/zest.specialpaste',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['zest'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )

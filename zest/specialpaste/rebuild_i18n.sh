8460fdec7342d7dcd4a3ccaa60071f178216d73b
Maurits van Rees <maurits@vanrees.org>
2011-11-02 16:17:00 +0100

    Translate the profile title and description manually.

--- zest/specialpaste/rebuild_i18n.sh
+++ zest/specialpaste/rebuild_i18n.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 # Run this script to update the translations.
 i18ndude rebuild-pot --pot locales/zest.specialpaste.pot --create zest.specialpaste .
+i18ndude rebuild-pot --pot locales/zest.specialpaste.pot --merge locales/manual.pot --create zest.specialpaste .
+
 i18ndude sync --pot locales/zest.specialpaste.pot $(find . -name 'zest.specialpaste.po')

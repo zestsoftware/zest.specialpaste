8c9295b00bca65ff0cc9fa70bd2746b9d543367e
Maurits van Rees <maurits@vanrees.org>
2011-11-02 16:07:19 +0100

    Added locales. Just one string and one Dutch translation currently.

---
+++ zest/specialpaste/rebuild_i18n.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# Run this script to update the translations.
+i18ndude rebuild-pot --pot locales/zest.specialpaste.pot --create zest.specialpaste .
+i18ndude sync --pot locales/zest.specialpaste.pot $(find . -name 'zest.specialpaste.po')

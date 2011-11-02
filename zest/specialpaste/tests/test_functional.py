cfb5f2f13d2184fb908583508a43791364cc4367
Maurits van Rees <maurits@vanrees.org>
2011-11-02 11:20:34 +0100

    Setup first basic tests, for the default Plone functionality.

--- zest/specialpaste/tests/test_functional.py
+++ zest/specialpaste/tests/test_functional.py
@@ -1,13 +1,38 @@
 import unittest2 as unittest
+
 from plone.app.testing import PLONE_INTEGRATION_TESTING
-from plone.app.testing import TEST_USER_NAME
+from plone.app.testing import TEST_USER_ID
 from plone.app.testing import setRoles
+from Products.CMFCore.utils import getToolByName
 
 
-class TestDocuments(unittest.TestCase):
+class TestNormalPaste(unittest.TestCase):
 
     layer = PLONE_INTEGRATION_TESTING
 
-    def testSetTitle(self):
+    def _makeOne(self, transition=None):
         portal = self.layer['portal']
-        self.assertEqual(portal.Title(), "New title")
+        setRoles(portal, TEST_USER_ID, ('Manager',))
+        new_id = portal.invokeFactory('Document', 'doc')
+        doc = portal[new_id]
+        if transition is not None:
+            wf_tool = getToolByName(portal, 'portal_workflow')
+            wf_tool.doActionFor(doc, transition)
+        return doc
+
+    def testCopyPastePrivate(self):
+        portal = self.layer['portal']
+        wf_tool = getToolByName(portal, 'portal_workflow')
+        wf_tool.setChainForPortalTypes(
+            ('Document',), 'simple_publication_workflow')
+        doc = self._makeOne()
+        self.assertEqual(wf_tool.getInfoFor(doc, 'review_state'), 'private')
+
+    def testCopyPastePublic(self):
+        portal = self.layer['portal']
+        wf_tool = getToolByName(portal, 'portal_workflow')
+        wf_tool.setChainForPortalTypes(
+            ('Document',), 'simple_publication_workflow')
+        doc = self._makeOne('publish')
+        wf_tool = getToolByName(portal, 'portal_workflow')
+        self.assertEqual(wf_tool.getInfoFor(doc, 'review_state'), 'published')

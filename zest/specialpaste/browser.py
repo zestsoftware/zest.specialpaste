df21a88c6435aa2e25a4dc178336535cc2c39f74
Maurits van Rees <maurits@vanrees.org>
2011-11-02 14:05:27 +0100

    Removed old call method from the browser view.

--- zest/specialpaste/browser.py
+++ zest/specialpaste/browser.py
@@ -30,42 +30,6 @@ class SpecialPaste(BrowserView):
 
     """
 
-    def UNUSED__call__(self):
-        """Do special paste.
-
-        This is mostly a copy of
-        Products/CMFPlone/skins/plone_scripts/object_paste.cpy
-
-        Possibly I could just call that.
-        """
-        context = aq_inner(self.context)
-        request = self.request
-        add_message = IStatusMessage(request).add
-        msg_type = 'error'  # Most messages are errors
-        if context.cb_dataValid():
-            # Mark the request with an interface that the event handlers
-            # react on.
-            alsoProvides(self.request, ISpecialPasteInProgress)
-            try:
-                context.manage_pasteObjects(self.request['__cp'])
-                transaction_note('Pasted content to %s' % (
-                    context.absolute_url()))
-                msg = PMF(u'Item(s) pasted.')
-                msg_type = 'info'
-            except ConflictError:
-                raise
-            except ValueError:
-                msg = PMF(u'Disallowed to paste item(s).')
-            except Unauthorized:
-                msg = PMF(u'Unauthorized to paste item(s).')
-            except:  # fallback
-                msg = PMF(u'Paste could not find clipboard content.')
-        else:
-            msg = PMF(u'Copy or cut one or more items to paste.')
-        add_message(msg, type=msg_type)
-        request.response.redirect(context.absolute_url())
-        return ''
-
     def __call__(self):
         """Do special paste.
 
@@ -73,11 +37,13 @@ class SpecialPaste(BrowserView):
         the request and let object_paste (or folder_paste) do the
         heavy lifting.
 
+        Oh, object_paste and folder_paste actually do exactly the same
+        thing, so that is easy.
+
         Possibly we should add a form in between that asks the user
         how special the paste must be (keep workflow state, keep local
         roles, etc).  For now we only handle the workflow state
         though.
-
         """
         context = aq_inner(self.context)
         alsoProvides(self.request, ISpecialPasteInProgress)
